342245ae18333a00e01f62a7fed40b1e854a7db8
aleklucatero <lucaalek99@gmail.com>
2024-04-08 18:23:17 -0700

    Making a simple Log in Log out
    In this example I use a simple log in log out button showing reactivity and logical statements.

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,75 @@
 <script>
-    import { add } from "date-fns";
+    // import { add } from "date-fns";
 
-	let count = 0;
-	function increment() {
-		count = count + 1
-	};
+	// let count = 0;
+	// function increment() {
+	// 	count = count + 1
+	// };
 
-	/*---------Addint string to list----------*/
+	// /*---------Addint string to list----------*/
 
-	let list = [`React`, `Vue`];
-	function handleClick() {
-		list = [...list, `Svelte`]
-	};
+	// let list = [`React`, `Vue`];
+	// function handleClick() {
+	// 	list = [...list, `Svelte`]
+	// };
 
-	/*--------Adding number to list----------*/
+	// /*--------Adding number to list----------*/
    
-	let items = [1, 2, 3, 4];
-	$: amount = items.length;
+	// let items = [1, 2, 3, 4];
+	// $: amount = items.length;
 
-	function addItems() {
-		items = [...items, items.length + 1]
-	}
+	// function addItems() {
+	// 	items = [...items, items.length + 1]
+	// }
 
-	/*--------Adding a string----------*/
-	let countries = [`USA`, `Mexico`];
-	$: newCountry = countries[countries.length - 1];
+	// /*--------Adding a string----------*/
+	// let countries = [`USA`, `Mexico`];
+	// $: newCountry = countries[countries.length - 1];
 
-	function addCountry() {
-		countries = [...countries, `Italy`];
-	}
+	// function addCountry() {
+	// 	countries = [...countries, `Italy`];
+	// }
 
-	/*--------Adding a string----------*/
-	let album = [
-		{track: `Track 1`, length: 180},
-		{track: `Track 2`, length: 240},
-		{track: `Track 3`, length: 280}
-	]
+	// /*--------Adding a string----------*/
+	// let album = [
+	// 	{track: `Track 1`, length: 180},
+	// 	{track: `Track 2`, length: 240},
+	// 	{track: `Track 3`, length: 280}
+	// ]
 
-	$: albumLength = getAlbumLength(album)
+	// $: albumLength = getAlbumLength(album)
 
-	function getAlbumLength(album) {
-		let lenghtSeconds = album.reduce((totalLength, currentValue) => {
-			return totalLength + currentValue.length
-		}, 0)
+	// function getAlbumLength(album) {
+	// 	let lenghtSeconds = album.reduce((totalLength, currentValue) => {
+	// 		return totalLength + currentValue.length
+	// 	}, 0)
 
-		let [minutes, seconds] =
-		(lenghtSeconds / 60)
-		.toFixed(2)
-		.toString()
-		.split('.')
+	// 	let [minutes, seconds] =
+	// 	(lenghtSeconds / 60)
+	// 	.toFixed(2)
+	// 	.toString()
+	// 	.split('.')
 
-		return { minutes, seconds };
+	// 	return { minutes, seconds };
+	// };
+
+	// function addTrack() {
+	// 	album = [...album, {track: 'Track 4', length: 420}]
+
+	// }
+
+	let twitteruser = {
+		loggedIn: false
 	};
-
-	function addTrack() {
-		album = [...album, {track: 'Track 4', length: 420}]
-	}
-
+	
+	function toggle() {
+		twitteruser.loggedIn = !twitteruser.loggedIn;
+	};
 
 </script>
 
 
-<p class="first-button">Clicked {count} {count === 1  ? `time` : `times`}</p>
+<!-- <p class="first-button">Clicked {count} {count === 1  ? `time` : `times`}</p>
 <button on:click={increment}>Click me</button>
 
 
@@ -75,21 +83,12 @@
 <button on:click={addCountry}>Add country</button>
 
 <p>Album length is {albumLength.minutes} minutes and {albumLength.seconds} seconds.</p>
-<button on:click={addTrack}>Add track</button>
+<button on:click={addTrack}>Add track</button> -->
 
+{#if twitteruser.loggedIn}
+	<button on:click={toggle}>Log in</button>
+{/if}
 
-
-<style>
-	p.first-button {
-		color: teal;
-	}
-
-	p.second-button {
-		color: red;
-	}
-
-	.items {
-		color: blue;
-	}
-
-</style>
+{#if !twitteruser.loggedIn}
+	<button on:click={toggle}>Log out</button>
+{/if}
